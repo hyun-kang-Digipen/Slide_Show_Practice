6bcbc20eed4a3e480a67846239330ac8908d44f3
hyun <hangam31@gmail.com>
2022-12-05 14:27:33 +0900

    add rolling Text in Timer.

--- Slide/View/TimerView.swift
+++ Slide/View/TimerView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct TimerView: View {
     @StateObject var data = TimerData()
-        
+    @State var animationRange: [Int] = []
+    
+    var font: Font = .largeTitle
+    var weight: Font.Weight = .regular
+    
     var body: some View {
         ZStack{
             SelectTimerScreen()
@@ -20,23 +24,54 @@ struct TimerView: View {
     @ViewBuilder
     func TimerScreen() -> some View {
         if data.IsTimerUp {
-            Text("\(data.selectedTime)")
-                .font(.system(size: 50, weight: .heavy))
-                .frame(width: 120, height: 100)
-                .foregroundColor(.orange)
-                .background {
-                    RoundedRectangle(cornerRadius: 30)
-                }
-                .onReceive(Timer.publish(every:1, on: .main, in: .common).autoconnect(), perform: { _ in
-                    if data.selectedTime != 0 {
-                        data.selectedTime -= 1
+            HStack(spacing: 10) {
+                ForEach(0..<animationRange.count, id: \.self) { index in
+                    Text("9")
+                        .font(font)
+                        .fontWeight(weight)
+                        .opacity(0)
+                    //.frame(width: 120, height: 100)
+                    //.foregroundColor(.orange)
+//                    .background {
+//                        RoundedRectangle(cornerRadius: 30)
+//                    }
+                    .overlay {
+                        GeometryReader { proxy in
+                            let size = proxy.size
+                            
+                            VStack(spacing: 0) {
+                                ForEach(0...9, id: \.self) { number in
+                                    Text("\(number)")
+                                        .font(font)
+                                        .fontWeight(weight)
+                                        .frame(width: size.width, height: size.height, alignment: .center)
+                                }
+                            }
+                            .offset(y: -CGFloat(animationRange[index]) * size.height)
+                        }
+                        .clipped()
                     }
-                })
-                .onTapGesture{
-                    data.IsTimerUp.toggle()
-                    data.selectedTime = data.time
                 }
+            }
+            .onAppear {
+                animationRange = Array(repeating: 0, count: "\(data.selectedTime)".count)
+                updateValue()
+            }
+            .onChange(of: data.selectedTime) { newValue in
+                updateValue()
+            }
+            .onReceive(Timer.publish(every: 1, on: .main, in: .common).autoconnect(), perform: { _ in
+                
+                if data.selectedTime != 0 {
+                    data.selectedTime -= 1
+                }
+            })
+            .onTapGesture{
+                data.IsTimerUp.toggle()
+                data.selectedTime = data.time
+            }
         }
+        
     }
     
     @ViewBuilder
@@ -61,6 +96,21 @@ struct TimerView: View {
                 }
             }
         }
+    }
+    
+    func updateValue() {
+        let stringValue = "\(data.selectedTime))"
+        
+        for (index, value) in zip(0..<animationRange.count, stringValue){
+            var fraction = Double(index) * 0.15
+            fraction = (fraction > 0.5 ? 0.5 : fraction)
+            
+            withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 1 + fraction, blendDuration: 1 + fraction)) {
+                animationRange[index] = (String(value) as NSString).integerValue
+            }
+        }
+        
+        
     }
 }
 
